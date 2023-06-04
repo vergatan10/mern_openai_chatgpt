e1d721d320b974fb3932266f0d45b8d5a53a2eeb
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-06-04 21:14:55 +0700

    first commit

---
+++ client/src/api/user.api.js
@@ -0,0 +1,35 @@
+import axiosClient from "./axios.client.js";
+
+export const userSignUp = async ({ username, password }) => {
+  try {
+    const response = await axiosClient.post("users/signup", {
+      username,
+      password,
+    });
+    return { response };
+  } catch (err) {
+    return { err };
+  }
+};
+
+export const userSignIn = async ({ username, password }) => {
+  try {
+    const response = await axiosClient.post("users/signin", {
+      username,
+      password,
+    });
+    return { response };
+  } catch (err) {
+    return { err };
+  }
+};
+
+export const userCheckTkn = async () => {
+  try {
+    const response = await axiosClient.get("users/check-token");
+
+    return { response };
+  } catch (err) {
+    return { err };
+  }
+};
